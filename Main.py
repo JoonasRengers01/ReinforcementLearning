78225b57155bc1a4d9f531a80c23f1b4726d0e66
JoonasRengers01 <joospy111@gmail.com>
2026-08-28 10:23:30 +0200

    prerun 20

--- Main.py
+++ Main.py
@@ -96,7 +96,7 @@ class snakeGame():
             if self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue < self.old_flood_fill:
                 reward += 50 - min(6,self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue)
             elif self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue > self.old_flood_fill:
-                reward += -50 - min(6,self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue)
+                reward += -60 - min(6,self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue)
             self.old_flood_fill = self.play_area.cells[self.snakeHead.segments[0].y][self.snakeHead.segments[0].x].floodFillValue
         else:
             reward += 400
--- rewardReader.py
+++ rewardReader.py
@@ -3,41 +3,63 @@ import matplotlib.pyplot as plt
 import os
 
 data = []
-
+filenames = []
 for file in os.listdir("RewardLogs"):
     if file.endswith(".csv"):        
         with open('RewardLogs/' + file, 'r') as f:                
             reader = csv.reader(f)
             data.append(list(reader))
+            filenames.append(file[7:-4])
+rewards = []
+episodecount = []
+framecount = []
+length = []
+print(filenames)
+for list in data:
+    rewards.append([float(row[0]) for row in list])
+    episodecount.append([int(row[1]) for row in list])
+    framecount.append([int(row[2]) for row in list])
+    length.append([int(row[3]) for row in list])
 
-rewards = [float(row[0]) for row in data]
-episodecount = [int(row[1]) for row in data]
-framecount = [int(row[2]) for row in data]
-length = [int(row[3]) for row in data]
+for i in range(len(rewards)):
+    print(f"max snake length for run {i+1}: {max(length[i])}, max reward: {max(rewards[i])}, total frames: {framecount[i][-1]}")
+    plt.scatter(episodecount[i], rewards[i], label=f"Run {i+1}")
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.title('Training Rewards')
+    plt.legend()
+    plt.savefig(f"Plots/TrainingRewards_{filenames[i]}.png")
+    plt.close()
 
+    running_average_length = []
+    window_size = 10
+    for j in range(len(length[i])):
+        if j < window_size:
+            running_average_length.append(sum(length[i][:j+1])/(j+1))
+        else:
+            running_average_length.append(sum(length[i][j-window_size:j+1])/(window_size))
+    plt.plot(episodecount[i], running_average_length,color='red', label=f"Run {i+1}")
+    plt.scatter(episodecount[i], length[i], label=f"Run {i+1}")
+    plt.xlabel('Episode')
+    plt.ylabel('Snake Length')
+    plt.title('Snake Length over Episodes')
+    plt.legend()
+    plt.savefig(f"Plots/SnakeLength_{filenames[i]}.png")
+    plt.close()
 
-plt.scatter(episodecount, rewards)
-plt.xlabel('Episode')
-plt.ylabel('Reward')
-plt.title('Training Rewards')
-plt.show()
+    framedelta = [framecount[i][j] - framecount[i][j-1] for j in range(1, len(framecount[i]))]
 
-plt.scatter(episodecount, length)
-plt.xlabel('Episode')
-plt.ylabel('Snake Length')
-plt.title('Snake Length over Episodes')
-plt.show()
+    plt.scatter(episodecount[i][1:], framedelta)
+    plt.xlabel('Episode')
+    plt.ylabel('Frame Delta')
+    plt.title('Frame Delta over Episodes')
+    plt.savefig(f"Plots/FrameDelta_{filenames[i]}.png")
+    plt.close()
 
-framedelta = [framecount[i] - framecount[i-1] for i in range(1, len(framecount))]
-
-plt.scatter(episodecount[1:], framedelta)
-plt.xlabel('Episode')
-plt.ylabel('Frame Delta')
-plt.title('Frame Delta over Episodes')
-plt.show()
-
-plt.scatter(length, rewards)
-plt.xlabel('Snake Length')
-plt.ylabel('Reward')
-plt.title('Reward over Snake Length')
-plt.show()
+    plt.scatter(length[i], rewards[i])
+    plt.xlabel('Snake Length')
+    plt.ylabel('Reward')
+    plt.title('Reward over Snake Length')
+    plt.legend()
+    plt.savefig(f"Plots/RewardOverSnakeLength_{filenames[i]}.png")
+    plt.close()
